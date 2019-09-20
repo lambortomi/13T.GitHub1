2b7fe25d3aec2bf6caa17764ffbe16668ebb1ab8
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 16:32:13 +0200

    Feladat 2

--- Hello_Git_Project1/Program.cs
+++ Hello_Git_Project1/Program.cs
@@ -15,11 +15,14 @@ namespace Hello_Git_Project1
             Console.WriteLine("Első github repoo");
             //Feladat 1
             Console.WriteLine("     Feladat 1");
+            //2. feladat
+            Console.WriteLine(" A program modosult úgy, hogy a tömb méretét bekérjük");
 
-            //int tombmeret = Convert.ToInt32(Console.ReadLine());
-            int tombmeret = 10;
+            Console.WriteLine("Kérem méretezze a tömböt -- Feladat 2"); //Opcionális
+            int tombmeret = Convert.ToInt32(Console.ReadLine()); //-->2. feladat
+            //int tombmeret = 10; 
 
-            int[] tomb10 = new int[10];
+            int[] tomb10 = new int[tombmeret]; // A program modosult úgy, hogy a tömb méretét bekérjük
             for (int i = 1; i < tomb10.Length+1; i++)
             {
                 Console.WriteLine("Kérek 10 számot");
@@ -33,7 +36,10 @@ namespace Hello_Git_Project1
             {
                 if (tomb10[i] > max) max = tomb10[i];
             }
-            Console.WriteLine("A legnagyobb szám a tömbben:" + max);
+            Console.WriteLine("A legnagyobb szám a tömbben: " + max);
+
+
+
 
         
             
