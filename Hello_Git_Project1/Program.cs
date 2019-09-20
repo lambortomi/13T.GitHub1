f980b0cb18decbaab78800e77225584133dee0d3
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 16:25:03 +0200

    Feladat 1

--- Hello_Git_Project1/Program.cs
+++ Hello_Git_Project1/Program.cs
@@ -10,11 +10,33 @@ namespace Hello_Git_Project1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("     Feladat 00");
             Console.WriteLine("Hello World");
-            Console.WriteLine("Első github repom");
+            Console.WriteLine("Első github repoo");
+            //Feladat 1
+            Console.WriteLine("     Feladat 1");
 
+            //int tombmeret = Convert.ToInt32(Console.ReadLine());
+            int tombmeret = 10;
 
+            int[] tomb10 = new int[10];
+            for (int i = 1; i < tomb10.Length+1; i++)
+            {
+                Console.WriteLine("Kérek 10 számot");
+                Console.WriteLine("Kérem írja be az "+ i +". számot");
+                tomb10[i-1] = Convert.ToInt32(Console.ReadLine());
 
+            }
+
+            int max = tomb10[0];
+            for (int i = 1; i < tombmeret; i++)
+            {
+                if (tomb10[i] > max) max = tomb10[i];
+            }
+            Console.WriteLine("A legnagyobb szám a tömbben:" + max);
+
+        
+            
 
             Console.ReadKey();
 
