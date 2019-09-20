43b7a823c54780bd2ac1a8684f42d6407af850a9
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 15:02:48 +0200

    Add project files.

---
+++ Hello_Git_Project1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Git_Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
