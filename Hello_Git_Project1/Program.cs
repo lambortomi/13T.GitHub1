670870875a99556253d5852c1f47d18243923208
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 16:35:24 +0200

    Feladat 3

--- Hello_Git_Project1/Program.cs
+++ Hello_Git_Project1/Program.cs
@@ -10,16 +10,20 @@ namespace Hello_Git_Project1
     {
         static void Main(string[] args)
         {
+            //1.Usertől kérjünk be 10 számot. Keressük meg, és írjuk ki a legnagyobbat!
+            //2.Módosítsuk a programot, hogy a usertől a darabszámot is bekérjük, ne csak 10 elemre működjön!
+            //3.Módosítsuk a programot, hogy a legnagyobb mellett a legkisebb elemet is megkeressük!
+
             Console.WriteLine("     Feladat 00");
             Console.WriteLine("Hello World");
             Console.WriteLine("Első github repoo");
-            //Feladat 1
+            //FELADAT 1
             Console.WriteLine("     Feladat 1");
-            //2. feladat
+            //FELADAT 2
             Console.WriteLine(" A program modosult úgy, hogy a tömb méretét bekérjük");
 
             Console.WriteLine("Kérem méretezze a tömböt -- Feladat 2"); //Opcionális
-            int tombmeret = Convert.ToInt32(Console.ReadLine()); //-->2. feladat
+            int tombmeret = Convert.ToInt32(Console.ReadLine()); //-->2. FELADAT
             //int tombmeret = 10; 
 
             int[] tomb10 = new int[tombmeret]; // A program modosult úgy, hogy a tömb méretét bekérjük
@@ -38,13 +42,16 @@ namespace Hello_Git_Project1
             }
             Console.WriteLine("A legnagyobb szám a tömbben: " + max);
 
+            //FELADAT 3
+            int min = tomb10[0];
+            for (int i = 1; i < tombmeret; i++)
+            {
+                if (tomb10[i] < min) min = tomb10[i];
+            }
+            Console.WriteLine("A legkisebb szám a tömbben:" + min);
 
 
-
-        
-            
-
-            Console.ReadKey();
+         Console.ReadKey();
 
         }
     }
